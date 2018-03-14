37ef904f0f7d3eb4ed8534204a3ea9365df54a39
Mikko Kotila <mailme@mikkokotila.com>
2018-03-14 18:03:16 +0200

    added simulator code

--- npvpy/simulate.py
+++ npvpy/simulate.py
@@ -1,1 +1,38 @@
+import warnings
+import pandas as pd
 
+from npvpy.main import NPV
+from npvpy.dist import dist
+
+def simulate(rounds, parameters, df=False, distribution=True):
+    
+    '''NPV Simulator
+    rounds =     the number of times simulation
+                 will be run
+                 
+    parameters = either a dictionary, or filename
+                 in string format.
+    
+    '''
+    
+    npv = NPV(parameters)
+    
+    out = []
+    
+    for i in range(rounds):
+        score = npv._calculate_npv()
+        out.append(score)
+    
+    if df is True:
+        out = pd.DataFrame(out)
+        out.columns = ['NPV']
+    
+    else:
+        out = np.array(out)
+        
+    if distribution is True:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            dist('NPV', scores, bins=50)
+            
+    return out
